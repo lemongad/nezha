0e7cca4b70ab04a835f7db81c1ee1c1bc17df7f1
lisa <104255899+lemongad@users.noreply.github.com>
2023-05-14 13:48:09 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@ apk add wget curl unzip
 read -p "域名: " NEZHA_SERVER &&
 read -p "端口: " NEZHA_PORT &&
 read -p "密钥: " NEZHA_KEY
+NEZHA_SERVER=${NEZHA_SERVER}
+NEZHA_PORT=${NEZHA_PORT}
+NEZHA_KEY=${NEZHA_KEY}
 wget https://github.com/naiba/nezha/releases/download/v0.14.12/nezha-agent_linux_386.zip
 unzip nezha-agent_linux_386.zip
 chmod +x nezha-agent
